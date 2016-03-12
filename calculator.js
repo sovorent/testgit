70422b4a2b0b15b112a401581d073faf4fb60985
bunyapont <bj.sunzz@gmail.com>
2016-03-12 14:51:46 +0700

    create mul

--- calculator.js
+++ calculator.js
@@ -7,28 +7,8 @@ function add(x,y){
 
 function sub(x,y) {
 	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
-}function sub(x,y) {
-	return x-y;
+}
+
+function mul(x,y){
+	return x/y;
 }
